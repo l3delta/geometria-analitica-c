2da479dcfb7ee7d5aa84e36f84cb71382ce2021f
Leandro Vieira dos Santos <leandrovieira2@hotmail.com>
2026-01-02 21:12:32 -0300

    Modificação inicial em ./src/ponto2d.c ./include/ponto2d.h

---
+++ include/ponto2d.h
@@ -0,0 +1,23 @@
+#ifndef PONTO2D_H
+#define PONTO2D_H
+
+/* 
+ * Representa um ponto no plano cartesiano ℝ²
+ */
+typedef struct {
+    double x;
+    double y;
+} Ponto2D;
+
+/* 
+ * Criação de ponto
+ */
+Ponto2D ponto2d_criar(double x, double y);
+
+/* 
+ * Distância entre dois pontos
+ */
+double ponto2d_distancia(Ponto2D a, Ponto2D b);
+
+#endif /* PONTO2D_H */
+
